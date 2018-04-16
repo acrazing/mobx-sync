a7211d1e15fe03e861c561279ef113625ab6298b
acrazing <joking.young@gmail.com>
2018-04-16 22:13:35 +0800

    fix(misc): to-json with undefined

--- src/to-json.ts
+++ src/to-json.ts
@@ -5,12 +5,16 @@
  * @author acrazing joking.young@gmail.com
  * @since 2018-01-06 17:12:54
  * @version 1.0.0
- * @desc misc.spec.ts
+ * @desc to-json.ts
  */
 
 export function toJSON(data: any, recursive = true) {
   if (recursive) {
-    return JSON.parse(JSON.stringify(data))
+  	const str = JSON.stringify(data)
+  	if(str === void 0) {
+  	  return void 0
+  	}
+    return JSON.parse(str)
   }
   if (!data || !('toJSON' in data)) {
     return data
