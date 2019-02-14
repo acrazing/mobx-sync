7a432005a7b5b68e8404ad8f70b85ec8ff9a81ef
acrazing <joking.young@gmail.com>
2019-02-14 15:39:03 +0800

    fix(core): use new template to init project

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright 2018 acrazing <joking.young@gmail.com>. All rights reserved.
+ * @since 2018-11-09 13:19:45
+ */
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
--- src/memory-storage.ts
+++ src/memory-storage.ts
@@ -8,36 +8,31 @@
  * @desc memory-storage.ts
  */
 
-import { SMap } from 'monofile-utilities';
+import { SMap } from 'monofile-utilities/lib/map';
 import * as util from 'util';
 import { SyncStorage } from './sync';
 
 export class MemoryStorage implements SyncStorage {
   private data: SMap<string> = {};
 
-  constructor (public debug = false) {
-  }
+  constructor(public debug = false) {}
 
-  getItem (key: string) {
-    this.debug && process.stderr.write(util.format(
-      'storage.get %s: %s\n',
-      key,
-      this.data[key],
-    ));
+  getItem(key: string) {
+    this.debug &&
+      process.stderr.write(
+        util.format('storage.get %s: %s\n', key, this.data[key]),
+      );
     return this.data.hasOwnProperty(key) ? this.data[key] : null;
   }
 
-  removeItem (key: string) {
+  removeItem(key: string) {
     this.debug && process.stderr.write(util.format('storage.remove %s\n', key));
     delete this.data[key];
   }
 
-  setItem (key: string, value: string) {
-    this.debug && process.stderr.write(util.format(
-      'storage.set %s: %s\n',
-      key,
-      value,
-    ));
+  setItem(key: string, value: string) {
+    this.debug &&
+      process.stderr.write(util.format('storage.set %s: %s\n', key, value));
     this.data[key] = value + '';
   }
 }
